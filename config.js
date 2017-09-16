6c78a414ed398801beb20fad4c89b0f18940db0a
Rory Caraher <rory@webtogether.com>
2017-09-16 17:02:22 +0100

    add techno playlist id

--- config.js
+++ config.js
@@ -1,5 +1,5 @@
 var config = {};
 config.youtube = {};
 config.youtube.key = ""; // Your YouTube API key here
-config.youtube.playlist = ""; // Your YouTube Playlist ID here
+config.youtube.playlist = "PLdONXlH_Zsprsgf_8YwkeIFihY_hTcqP-"; // Your YouTube Playlist ID here
 module.exports = config;
