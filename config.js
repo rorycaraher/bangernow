72fb6b789c00f24d012cc040b76ab0387387704a
Rory Caraher <rory@webtogether.com>
2017-09-16 16:51:21 +0100

    remove my keys

--- config.js
+++ config.js
@@ -1,5 +1,5 @@
 var config = {};
 config.youtube = {};
-config.youtube.key = "AIzaSyBB6oieIYIjkGWVmRzyVLwrymR_-wzD4KM"; // Your YouTube API key here
-config.youtube.playlist = "PLdONXlH_Zsprsgf_8YwkeIFihY_hTcqP-"; // Your YouTube Playlist ID here
+config.youtube.key = ""; // Your YouTube API key here
+config.youtube.playlist = ""; // Your YouTube Playlist ID here
 module.exports = config;
