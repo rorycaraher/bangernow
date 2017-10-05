1e97951b696e0ed31cabf8c5475642cfc7b2ca23
Rory Caraher <rory@webtogether.com>
2017-10-05 17:29:47 +0100

    remove my api key

--- config.js
+++ config.js
@@ -1,6 +1,6 @@
 module.exports = {
     youtube: {
-         api_key: "AIzaSyBB6oieIYIjkGWVmRzyVLwrymR_-wzD4KM", // your API key here
+         api_key: "", // your API key here
          playlist_id: "PLdONXlH_Zsprsgf_8YwkeIFihY_hTcqP-" // your playlist ID here
     }
 }
